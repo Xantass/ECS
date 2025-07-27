271b41f5a00eba53ce21a6aaca46042b185866ad
Xantas <theo.fouque@epitech.eu>
2025-07-27 16:16:42 +0200

    refactor: comment the gamepad handling logic in InputSystem

--- ecs/src/systems/InputSystem.cpp
+++ ecs/src/systems/InputSystem.cpp
@@ -14,26 +14,21 @@ void InputSystem::update(Registry &registry, float /*dt*/)
 
         vel.dx = 0;
         vel.dy = 0;
-
-        for (int i = 0; i < MAX_GAMEPADS; i++) {
-            if (IsGamepadAvailable(i)) {
-                std::cout << "Gamepad: " << i << std::endl;
-            }
-        }
         
-        if (gamepad.id != -1) {
-            std::cout << "Gamepad: " << gamepad.id << std::endl;
-            if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_DOWN))
-                registry.getEventBus().emit(InputEvent{InputType::KeyRight, entity});
-            if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_UP))
-                registry.getEventBus().emit(InputEvent{InputType::KeyLeft, entity});
-            if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_LEFT))
-                registry.getEventBus().emit(InputEvent{InputType::KeyDown, entity});
-            if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_RIGHT))
-                registry.getEventBus().emit(InputEvent{InputType::KeyUp, entity});
-            if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_DOWN))
-                registry.getEventBus().emit(InputEvent{InputType::KeySpace, entity});
-        }
+        // if (gamepad.id != -1) {
+        //     std::cout << "Gamepad: " << gamepad.id << std::endl;
+        //     if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_DOWN))
+        //         registry.getEventBus().emit(InputEvent{InputType::KeyRight, entity});
+        //     if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_UP))
+        //         registry.getEventBus().emit(InputEvent{InputType::KeyLeft, entity});
+        //     if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_LEFT))
+        //         registry.getEventBus().emit(InputEvent{InputType::KeyDown, entity});
+        //     if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_RIGHT))
+        //         registry.getEventBus().emit(InputEvent{InputType::KeyUp, entity});
+        //     if (IsGamepadButtonPressed(gamepad.id, GAMEPAD_BUTTON_RIGHT_FACE_DOWN))
+        //         registry.getEventBus().emit(InputEvent{InputType::KeySpace, entity});
+        // }
+
         if (IsKeyDown(KEY_RIGHT))
             registry.getEventBus().emit(InputEvent{InputType::KeyRight, entity});
         if (IsKeyDown(KEY_LEFT))
